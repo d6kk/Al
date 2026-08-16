56ad5b25ddb6e9a95f2041be92cc5cf61e67efbd
Dk <82993532+d6kk@users.noreply.github.com>
2026-08-16 19:44:19 +0530

    Delete Script.sh

--- Script.sh
+++
@@ -1,3 +0,0 @@
-# This file was removed and its content merged into file.sh
-# Original file: Script.sh
-# To actually delete this file from the repository, remove it via GitHub UI or git with appropriate permissions.
